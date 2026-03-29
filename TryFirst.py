aacb551bfcef4e5e53730db8ea3ee86ed2b1ee30
SyedNajeeb-Git <najeebulhaquesyed@gmail.com>
2026-03-29 21:28:19 +0530

    Update TryFirst.py

--- TryFirst.py
+++ TryFirst.py
@@ -1,19 +1,22 @@
 import mysql.connector 
+import tkinter
 
 from time import sleep
 
 print('Assalam Alekum')
-
 print('I was able to import sql conenctor')
 print('Walekum Salaam')
 print('Ramazan Mubarak')
 
+try:
 
-cn = mysql.connector.connect(auth_plugin='mysql_native_password', host='localhost', user='najeeb', passwd='Njb@Mysql25', database='myfirstschema')
+    cn = mysql.connector.connect(auth_plugin='mysql_native_password', host='localhost', user='najeeb', passwd='Njb@Mysql25', database='myfirstschema')
+    cursor = cn.cursor()
+except Exception as e:
+    print('Something is not right : ', e)
+    exit()
 
-cursor = cn.cursor()
-
-
+    
 cursor.execute('SELECT * FROM prod_codes')
 for (productCode,prodcode_desc, prod_cat,prc) in cursor:
     print("Product Code: {} -- Product Code Desc: {}  ".format( productCode, prodcode_desc))
